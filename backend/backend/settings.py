6d2f6bfcb2fd4acc07800cdcea5052923da91f1a
Wilson Mutua <willymathuva@gmail.com>
2026-01-29 10:25:25 +0300

    Installed Apps changed

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -31,13 +31,17 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    "django.contrib.admin",
-    "django.contrib.auth",
-    "django.contrib.contenttypes",
-    "django.contrib.sessions",
-    "django.contrib.messages",
-    "django.contrib.staticfiles",
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+
+    'rest_framework',
+    'routing',
 ]
+
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
